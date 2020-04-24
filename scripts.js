98a92ef48a92cb88c8f4cedb3ddb6698fff948e7
Drem4d <56813450+Drem4d@users.noreply.github.com>
2020-04-23 21:39:03 -0500

    Intento 02

---
+++ scripts.js
@@ -0,0 +1,25 @@
+const form = document.getElementById('form')
+const characters = document.getElementById('characters')
+const table = document.getElementById('table')
+
+
+const getData = (id) => {
+    let xhr
+    
+    if (id == undefined) {
+        xhr.open('GET', 'marvel.php')
+
+        xhr.addEventListener('load', (data) => {
+            const dataJSON = JSON.parse(data.target.response)
+            console.log(dataJSON)
+        
+          })} else {
+
+    
+    }
+    
+    xhr.send()
+}
+
+getData()
+
